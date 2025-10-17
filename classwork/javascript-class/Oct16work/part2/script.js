64aba145fbc21310d7f558ce42943b96d20108b3
anueban49 <anudari.enkhbat049@gmail.com>
2025-10-17 14:02:01 +0800

    Oct17changes

--- classwork/javascript-class/Oct16work/part2/script.js
+++ classwork/javascript-class/Oct16work/part2/script.js
@@ -1,33 +1,14 @@
-// const plus = document.getElementById("plus");
-// const minus = document.getElementById("minus");
-// const num = document.getElementById("num_indicator");
-// function increment(num) {
-//     num++;
-// }
-// function substraction(num) {
-//     num--;
-//     return num;
-// }
-// plus.addEventListener("click", () => {
-//     num++;
-//     num.textContent = num;
-// });
-// minus.addEventListener("click", () => {
-//     num--;
-//     num.textContent = num;
-// });
-
-
-let count = document.getElementById("num_indicator");
+let num = document.getElementById("num_indicator");
 const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
+let count = 0;
 
-    plus.addEventListener("click", () => {
-      count++;
-      count.textContent = count;
-    });
+plus.addEventListener("click", () => {
+  count++;
+  num.textContent = count;
+});
 
-    minus.addEventListener("click", () => {
-      count--;
-      count.textContent = count;
-    });
+minus.addEventListener("click", () => {
+  count--;
+  num.textContent = count;
+});
---
+++ classwork/javascript-class/Oct17work/fuckidandclass/script.js
@@ -0,0 +1,15 @@
+const message = document.getElementById("message_box");
+
+let messageUpdated = "";
+
+function orderCake() {
+  message.textContent = "Cake";
+}
+
+function orderCandy() {
+  message.textContent = "Candy";
+}
+
+function orderIcecream() {
+  message.textContent = "Icecream";
+}
