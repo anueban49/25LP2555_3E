f4791c6877a934b4b3601d1d6f260ee264b29883
anueban49 <anudari.enkhbat049@gmail.com>
2025-10-22 13:53:59 +0800

    oct22work

--- classwork/javascript-class/Oct20work/script.js
+++ classwork/javascript-class/Oct20work/script.js
@@ -1,6 +1,40 @@
-const addBtn = document.getElementById("addItem");
-const list = document.getElementById("ListItems").value = [];
-const input = document.getAnimations("input").value;
-function addList() {
-    document.body.main.querySelector("listItems")
+const addBtn = document.getElementById("add");
+let listItems = document.getElementById("listItems");
+const input = document.getElementById("input");
+let content = [];
+addBtn.addEventListener("click", () => {
+    const value = input.value.trim();
+    if (value === "") return;
+    content.push(value);
+    listItems.innerHTML = "";
+    content.forEach(value => {
+        const div = document.createElement("div");
+        div.className = "item";
+        div.attributes = "data-category";
+        div.innerHTML = '<input class="checkbox" type="checkbox"/> <p class="task">' + value + '</p>'
+        listItems.appendChild(div);
+    })
+    input.value = "";
+})
+const taskItem = {
+    checkbox: false,
+    category: "Incomplete",
 }
+const checkbox = document.getElementsByClassName("checkbox");
+if (checkbox !== null) {
+    checkbox.addEventListener("change", () => {
+        if (checkbox.checked) {
+            item.setAttribute("data-category", "done");
+        } else {
+            i
+        }
+    })
+}
+// checkbox.addEventListener("click", => {
+
+// })
+// const showAllBtn = document.getElementById("showAllBtn");
+// const showAll = document.getElementsByClassName("showAll")
+// showAllBtn.addEventListener("click", => {
+//     listItems.style.display = document.getElementById("showAll");
+// })
