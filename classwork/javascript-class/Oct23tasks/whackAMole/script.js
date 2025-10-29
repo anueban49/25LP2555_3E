05b488879e7d0e719adf5ff4b4ba2311c1c0e5dd
anueban49 <anudari.enkhbat049@gmail.com>
2025-10-29 09:34:28 +0800

    ..

--- classwork/javascript-class/Oct23tasks/whackAMole/script.js
+++ classwork/javascript-class/Oct23tasks/whackAMole/script.js
@@ -2,15 +2,22 @@ const startBtn = document.getElementById("start");
 const holes = document.querySelectorAll(".holes");
 const moles = document.querySelectorAll(".mole");
 const scoreDisplay = document.getElementById("score");
-
+const timeDisplay = document.getElementById("timeDisplay");
+let time = 10000;
+const seconds = time / 1000;
+const min = 
+console.log(seconds);
 // console.log(moles.length);
-const gameplay = () => {
+
+// let stopGamePlay = false;
+
+function gameplay() {
     setInterval (() => {
         const randomIndex = Math.floor(Math.random() * moles.length)
         const mole = moles[randomIndex]
-        mole.style.scale = 1;
+        mole.classList.add("moleStyle");
         setTimeout(() => {
-            mole.style.scale = "0";
+            mole.classList.remove("moleStyle");
         }, 900)
     }, 1200)
     
@@ -18,12 +25,21 @@ const gameplay = () => {
         mole.addEventListener("click", () => {
             let score = 0;
             score += 1;
-            return score;
+            console.log(score);
+            scoreDisplay.textContent = score;
+            mole.style.backgroundColor = "red";
+            setTimeout(() => {
+                mole.style.backgroundColor = "rgb(165, 149, 115)"
+            }, 500)
         }
+        
     ) 
     ))
-    scoreDisplay.textContent = score;
+    
 }
+// const timeGiven = setTimeout(() => {
+//     break gameplay();
+// }, 10000)
 
 
 
